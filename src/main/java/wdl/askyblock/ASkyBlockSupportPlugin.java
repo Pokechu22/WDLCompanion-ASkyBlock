f9b42c87b7bcea5cb177b9a336c2968f298a39b9
Pokechu22 <Pokechu022@gmail.com>
2016-03-19 19:40:09 -0700

    Add MCStats support

--- src/main/java/wdl/askyblock/ASkyBlockSupportPlugin.java
+++ src/main/java/wdl/askyblock/ASkyBlockSupportPlugin.java
@@ -3,8 +3,14 @@ package wdl.askyblock;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
+import org.mcstats.Metrics.Graph;
+import org.mcstats.Metrics.Plotter;
+
+import com.wasteofplastic.askyblock.ASkyBlockAPI;
 
 import wdl.RangeGroupTypeRegistrationEvent;
+import wdl.range.IRangeProducer;
 
 /**
  * Plugin that provides simple support for ASkyBlock chunk overrides.
@@ -15,6 +21,35 @@ public class ASkyBlockSupportPlugin extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
+		
+		try {
+			class StringPlotter extends Plotter {
+				public StringPlotter(String str) {
+					super(str);
+				}
+				
+				@Override
+				public int getValue() {
+					return 1;
+				}
+			}
+			
+			Metrics metrics = new Metrics(this);
+			
+			Graph asbVersionGraph = metrics.createGraph("askyblockVersion");
+			String askyblockVersion = getProvidingPlugin(ASkyBlockAPI.class)
+					.getDescription().getFullName();
+			asbVersionGraph.addPlotter(new StringPlotter(askyblockVersion));
+			
+			Graph wdlcVersionGraph = metrics.createGraph("wdlcompanionVersion");
+			String wdlcVersion = getProvidingPlugin(IRangeProducer.class)
+					.getDescription().getFullName();
+			wdlcVersionGraph.addPlotter(new StringPlotter(wdlcVersion));
+			
+			metrics.start();
+		} catch (Exception e) {
+			getLogger().warning("Failed to start PluginMetrics :(");
+		}
 	}
 	
 	@EventHandler
